40d0be1303751db9005e7507e0d33ab90b3c1897
fjrXTR <fajarslebew31@gmail.com>
2024-06-17 16:33:25 +0800

    LG8n: extract-files: Fixup libsink
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>
    Signed-off-by: dmpstr <aubrey.jmps@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -67,7 +67,7 @@ function blob_fixup {
         vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
             "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
             ;;
-        lib64/libsink.so)
+        system/lib64/libsink.so)
             "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
             ;;
         vendor/lib*/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so)
