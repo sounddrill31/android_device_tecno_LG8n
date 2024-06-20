190398af7160417bbc55542748841f9b20e629d8
AndroidHQ254 <d.murimi14@gmail.com>
2024-06-21 02:19:30 +0800

    LG8n: Link older libhidlbase & libbinder for PQ service
    F linker  : CANNOT LINK EXECUTABLE /vendor/bin/hw/vendor.mediatek.hardware.pq@2.2-service: cannot locate symbol _ZN7android8String16aSEOS0_ referenced by /apex/com.android.vndk.v34/lib64/libhidlbase.so...

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,11 @@ function blob_fixup {
         vendor/lib*/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
+        vendor/bin/hw/vendor.mediatek.hardware.pq@2.2-service)
+            "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v31.so" "${2}"
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v31.so" "${2}"
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v31.so" "${2}"
+            ;;
         vendor/lib/hw/audio.primary.mt6789.so)
             ;&
         vendor/lib64/hw/audio.primary.mt6789.so)
