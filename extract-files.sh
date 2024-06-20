f4970e71a3ec72ac848af84e5b581f6ba1d35644
Shirayuki39 <lorddemecrius83@proton.me>
2024-06-21 00:02:35 +0800

    LG8n: Use stock lights HAL
    Told ya' this will get reintroduced later
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,9 @@ function blob_fixup {
         vendor/bin/hw/android.hardware.vibrator-service.mediatek)
             "$PATCHELF" --replace-needed "android.hardware.vibrator-V2-ndk_platform.so" "android.hardware.vibrator-V2-ndk.so" "$2"
             ;;
+        vendor/bin/hw/android.hardware.lights-service.mediatek)
+            "$PATCHELF" --replace-needed "android.hardware.light-V1-ndk_platform.so" "android.hardware.light-V1-ndk.so" "$2"
+            ;;
         vendor/bin/hw/mt6789/camerahalserver)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v31.so" "${2}"
             "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v31.so" "${2}"
