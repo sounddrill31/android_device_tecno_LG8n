334b03326ee2e3d925a2ed87a5ddbc056867e763
Shirayuki39 <lorddemecrius83@proton.me>
2024-07-17 07:06:14 +0800

    LG8n: Import LG8n Audio and Bluetooth Blobs
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -45,20 +45,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.0-impl \
+    android.hardware.audio.service \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio.service
+    android.hardware.audio@7.0-impl \
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
-    audio.bluetooth.default \
-    android.hardware.bluetooth.audio-impl
+    audio.primary.default \
+    audio.r_submix.default \
+    audio.usb.default \
+    audio_policy.stub
 
 PRODUCT_PACKAGES += \
-    libaudiofoundation.vendor \
-    libbluetooth_audio_session \
     libalsautils \
-    libnbaio_mono \
-    libtinycompress \
+    libaudiofoundation.vendor \
     libdynproc \
     libhapticgenerator
 
@@ -72,7 +72,10 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1.vendor
+    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth@1.1.vendor \
+    audio.bluetooth.default \
+    libbluetooth_audio_session
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
