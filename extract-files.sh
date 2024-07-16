afc7e6f4268ab129994bf3e54e3735d3ae169c84
fjrXTR <fajarslebew31@gmail.com>
2024-07-17 07:28:11 +0800

    LG8n: link older libutils for audio.primary.mt6789
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,11 @@ function blob_fixup {
         vendor/bin/hw/android.hardware.lights-service.mediatek)
             "$PATCHELF" --replace-needed "android.hardware.light-V1-ndk_platform.so" "android.hardware.light-V1-ndk.so" "$2"
             ;;
+        vendor/lib/hw/audio.primary.mt6789.so)
+            ;&
+        vendor/lib64/hw/audio.primary.mt6789.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v31.so" "${2}"
+            ;;
         vendor/bin/hw/mt6789/camerahalserver)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v31.so" "${2}"
             "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v31.so" "${2}"
