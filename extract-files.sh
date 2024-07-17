617b679323fd5e90a948a981d37f4ed70dce38d5
SamarV-121 <samarvispute121@pm.me>
2024-07-17 14:35:13 +0800

    LG8n: Use libutils-v32.so for thermal HAL
    F libc    : Fatal signal 6 (SIGABRT), code -1 (SI_QUEUE) in tid 736 (ThermalWatcherT), pid 723 (thermal@2.0-ser)
    F DEBUG   : *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
    F DEBUG   : Build fingerprint: 'Redmi/selene/selene:11/RP1A.200720.011/V12.5.20.0.RKUMIXM:user/release-keys'
    F DEBUG   : Revision: '0'
    F DEBUG   : ABI: 'arm64'
    F DEBUG   : Timestamp: 2024-03-01 22:36:55.466668923+0700
    F DEBUG   : Process uptime: 1s
    F DEBUG   : Cmdline: /vendor/bin/hw/android.hardware.thermal@2.0-service.mtk
    F DEBUG   : pid: 723, tid: 736, name: ThermalWatcherT  >>> /vendor/bin/hw/android.hardware.thermal@2.0-service.mtk <<<
    F DEBUG   : uid: 1000
    F DEBUG   : signal 6 (SIGABRT), code -1 (SI_QUEUE), fault addr --------
    F DEBUG   : Abort message: 'new_alloc_size overflow'
    
    Change-Id: I36280695b618ba1fc8a2a6cbeb8fa2f0a3af4fc4
    Signed-off-by: Mikazuuu07 <mikazuuu07@komodo-os.my.id>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -103,6 +103,9 @@ function blob_fixup {
         vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
         "${PATCHELF}" --replace-needed "android.hardware.power-V2-ndk_platform.so" "android.hardware.power-V2-ndk.so" "${2}"
         ;;
+       vendor/lib*/hw/android.hardware.thermal@2.0-impl.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            ;;
     esac
 }
 
