ed388ee2e3382fb3d7b0ffddae8fa269afdc6483
basamaryan <basam.aryan@gmail.com>
2024-07-25 23:45:22 +0800

    LG8n:  Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -430,9 +430,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@2.0.vendor
 
-# Updatable APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
