92c8b70e959829d32d9afdc3d08b9ddc888845f3
Shirayuki39 <lorddemecrius83@proton.me>
2024-07-26 00:54:23 +0800

    LG8n: Add `hardware/mediatek/libmtkperf_client` to Soong namespaces
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -453,6 +453,7 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/mediatek \
+    hardware/mediatek/libmtkperf_client \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/transsion
