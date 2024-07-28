96cc297e68e9d2607648e12c9ebbe328a9ec56ac
Shirayuki39 <lorddemecrius83@proton.me>
2024-07-28 23:03:35 +0800

    LG8n: Stock Vibrator HAL's revenge
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -505,10 +505,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libui_shim.vendor
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.transsion
-
 # Wi-Fi
 PRODUCT_PACKAGES += \
     libwifi-hal-wrapper \
