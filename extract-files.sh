a86a0bdd39d29dbd342f1694d89c94569a9a7a18
Shirayuki39 <lorddemecrius83@proton.me>
2024-07-29 02:44:03 +0800

    Revert "LG8n: Stock Vibrator HAL's revenge"
    This reverts commit 96cc297e68e9d2607648e12c9ebbe328a9ec56ac.

--- extract-files.sh
+++ extract-files.sh
@@ -59,9 +59,6 @@ function blob_fixup {
         vendor/lib*/mediadrm/libwvdrmengine.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
-        vendor/bin/hw/android.hardware.vibrator-service.mediatek)
-            "$PATCHELF" --replace-needed "android.hardware.vibrator-V2-ndk_platform.so" "android.hardware.vibrator-V2-ndk.so" "$2"
-            ;;
         vendor/bin/hw/android.hardware.lights-service.mediatek)
             "$PATCHELF" --replace-needed "android.hardware.light-V1-ndk_platform.so" "android.hardware.light-V1-ndk.so" "${2}"
             ;;
